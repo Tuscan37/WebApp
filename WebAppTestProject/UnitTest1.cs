bb4e07e406c80bca4608bb51be8d6109c806f76b
jakub <jakuble@onet.pl>
2024-04-21 13:08:13 +0200

    Utworzono środowisko do testów. Testy tworzą dedykowaną bazę danych i usuwają ją po zakończeniu testów. Postgres nie lubi DateTime.Now, coś tam timestamp, dopóki tego bardziej nie ogarnę, to będzię używane DateTime.UtcNow

--- WebAppTestProject/UnitTest1.cs
+++ WebAppTestProject/UnitTest1.cs
@@ -1,9 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace WebAppTestProject;
-public class UnitTest1
+using WebAppSzczegielniak.Data;
+public class UnitTest1 : IDisposable
 {
-    [Fact]
-    public void Test1()
+    private const string ConnectionString = "Server=127.0.0.1;Port=5432;Database=webappszczegielniaktest;User Id=appuser;Password=1234";
+    private readonly ApplicationDbContext _context;
+
+    public UnitTest1()
     {
-        Assert.True(true);
+        _context = new ApplicationDbContext(
+            new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseNpgsql(ConnectionString)
+                .Options);
+        _context.Database.EnsureDeleted();
+        _context.Database.Migrate();
+
+    }
+
+    public void Dispose()
+    {
+        _context.Database.EnsureDeleted();
+        _context.Dispose();
+    }
+    [Fact]
+    public async Task Test1()
+    {
+        _context.Users.Add(new User
+        {
+            Username = "Jan",
+            Password = "1234"
+        });
+        var project = _context.Projects.Add(new Project
+        {
+            ProjectName = "WebApp",
+            CreationDateTime = DateTime.UtcNow,
+            DeadlineDateTime = DateTime.UtcNow.AddDays(60),
+            Description = "cool app"
+        });
+        _context.Assignments.Add(new Assignment
+        {
+            Project = project.Entity,
+            Description = "add some feature",
+            DeadlineDateTime = DateTime.UtcNow.AddDays(7),
+            Priority = 1000,
+            Name = "Add Some Feature!!"
+
+        });
+        await _context.SaveChangesAsync();
+
+        var assignmentInDb = await _context.Assignments.Include(a =>a.Project).FirstAsync();
+        Assert.Equal(project.Entity.Id,assignmentInDb.Project.Id);
+
     }
 }
